1b8d1178ec89d7f7ae20800c8577298a9d671764
goodbyebada <goodbyebada@naver.com>
2023-12-26 20:09:26 +0900

    환경 세팅

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index");
+});
+router.get("/test", function (req, res, next) {
+  const path = require("path");
+  const options = { root: path.join(__dirname, "../views") };
+  res.sendFile("htmlTest.html", options);
 });
 
 module.exports = router;
