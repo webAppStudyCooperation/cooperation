8e63dbccab18282d96b54bff72a51c9b38ed3a16
goodbyebada <goodbyebada@naver.com>
2023-12-26 20:16:19 +0900

    commit

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   res.render("index");
 });
-router.get("/test", function (req, res, next) {
-  const path = require("path");
-  const options = { root: path.join(__dirname, "../views") };
-  res.sendFile("htmlTest.html", options);
-});
+// router.get("/test", function (req, res, next) {
+//   const path = require("path");
+//   const options = { root: path.join(__dirname, "../views") };
+//   res.sendFile("htmlTest.html", options);
+// });
 
 module.exports = router;
