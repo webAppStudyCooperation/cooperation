8d53bc02c0d6234d7aeb2988955ad528c4e6f9ee
1004gite <1004gite@naver.com>
2024-01-15 23:24:16 +0900

    소켓 추가

---
+++ public/ts/socketTest.ts
@@ -0,0 +1,29 @@
+// import { io } from "socket.io/client-dist/socket.io.js";
+// import { test } from "socket.io-client/dist/socket.io.js"
+import { baseURL } from "./config.js";
+
+// const socketClient = io(baseURL);
+
+// socketClient.on("connect", () => {
+//   console.log("connection server");
+// });
+
+// $(function () {
+//     var socket = io();
+//     socket.emit('joinRoom', "room1");
+//     $('form').submit(function(){
+//         socket.emit('chatting', {
+//             roomName: "room1",
+//             msg: $('#m').val()
+//         });
+//         $('#m').val('');
+//         return false;
+//     });
+//     socket.on('broadcast', function(msg){
+//         $('#messages').append($('<li>').text(msg));
+//     });
+// });
+
+function joinRoom() {
+
+}
